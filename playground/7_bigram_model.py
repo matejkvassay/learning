d0be018b3ca4c2f8a635c1c7906b2ebeefeeb670
matej.kvassay@telekom.com <matej.kvassay@telekom.com>
2024-06-10 17:10:40 +0200

    bigram model

--- playground/7_bigram_model.py
+++ playground/7_bigram_model.py
@@ -1,5 +1,5 @@
 from mkvlib.char_tokenizer import CharTokenizer
-from mkvlib.train_utils import generate_nwp_batch
+from mkvlib.train_utils import rand_nwp_batch
 import torch
 from torch import nn
 from torch.nn import functional as F
@@ -14,26 +14,34 @@ with open(DATASET_PATH, 'r') as f:
     text = f.read().strip()
 
 tokenizer = CharTokenizer(' ')
-tokens = tokenizer.fit_transform(text)
+indices = tokenizer.fit_transform(text)
 
-train_size = int(TRAIN_SPLIT * len(tokens))
-tok_train = tokens[:train_size]
-tok_test = tokens[train_size:]
+train_size = int(TRAIN_SPLIT * len(indices))
+x_train = torch.tensor(indices[:train_size])
+x_test = torch.tensor(indices[train_size:])
 
-assert len(tokenizer) < 125
-x_train = torch.tensor(tok_train, dtype=torch.int8)
-x_test = torch.tensor(tok_test, dtype=torch.int8)
+x, y = rand_nwp_batch(x_train, BATCH_SIZE, CONTEXT_LENGTH)
 
-x, y = generate_nwp_batch(x_train, BATCH_SIZE, CONTEXT_LENGTH)
-print(x)
-print(y)
 
-#
-# class BigramModel(nn.Module):
-#     def __init__(self, vocab_size):
-#         super().__init__()
-#         self.emb = nn.Embedding(num_embeddings=vocab_size, embedding_dim=vocab_size)
-#
-#     def forward(self, token_idx, targets):
-#         logits = self.emb(token_idx)
-#         batch_dim,
+class BigramModel(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.emb = nn.Embedding(num_embeddings=vocab_size, embedding_dim=vocab_size)
+
+    def forward(self, X, Y=None):
+        logits = self.emb(X)  # dims: (Batch size , Time (context) 8, Channel (vocab size))
+        if Y is None:
+            return logits,
+
+        B, T, C = logits.shape
+        logits = logits.view((B * T, C))
+        Y = Y.view(-1)
+        loss = F.cross_entropy(logits, Y)
+        return logits, loss
+
+    def generate(self, x, max_tokens):
+        for _ in range()
+
+
+m = BigramModel(len(tokenizer))
+print(m(x, y))
--- src/mkvlib/train_utils.py
+++ src/mkvlib/train_utils.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def generate_nwp_batch(tokens, batch_size, context_length):
+def rand_nwp_batch(tokens, batch_size, context_length):
     indices = torch.randint(tokens.shape[0] - context_length - 1, (batch_size,))
     x = torch.stack(tuple(tokens[i:i + context_length] for i in indices))
     y = torch.stack(tuple(tokens[i + 1:i + context_length + 1] for i in indices))
